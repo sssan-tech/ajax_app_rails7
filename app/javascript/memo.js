8b27706a3d306c932c160173f08d1a6bfff8cd1c
sssan-tech <sasaki.re2019@gmail.com>
2024-11-05 20:25:32 +0900

    PostgreSQLの設定

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -15,13 +15,13 @@ const buildHTML = (XHR) => {
 function post (){
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
-    e.preventDefault();
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
-
+    e.preventDefault();
+    
     XHR.onload = () => {
       if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
